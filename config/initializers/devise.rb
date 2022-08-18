f3d48ec610333edc0d540c46ee7ad208e926e989
eric_norcross <eric@base10creations.com>
2022-08-18 16:47:32 -0700

    precompiled assets

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,7 +311,7 @@ Devise.setup do |config|
 
   # JWT Devise
   config.jwt do |jwt|
-    jwt.secret = ENV.fetch('DEVISE_JWT_SECRET_KEY')
+    jwt.secret = ENV.fetch('DEVISE_JWT_SECRET_KEY') { '' }
     jwt.dispatch_requests = [
       ['POST', %r{^/users/login$}]
     ]
