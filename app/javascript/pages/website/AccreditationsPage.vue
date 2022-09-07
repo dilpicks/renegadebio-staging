ea72b8a09406272f4c663f2a8e6600a1039b7c62
eric_norcross <eric@base10creations.com>
2022-09-07 14:23:31 -0700

    added privacy policy pages

--- app/javascript/pages/website/AccreditationsPage.vue
+++ app/javascript/pages/website/AccreditationsPage.vue
@@ -1,25 +1,97 @@
 <template>
   <div class="page">
-    <section>
+    <section class="section policy accreditations">
       <div class="container">
-        <h2 class="text-center">You are visiting the {{ pageName }} page!</h2>
+        <!-- eslint-disable -->
+        <div id="accreditations-and-certifications" class="sub-section copy-block">
+          <h3 class="h3">Accreditations &amp; Certifications</h3>
+          <p class="p4">
+            RenegadeXBio, PBC developed the renegadeXP<sup>&trade;</sup> SARS-CoV-2 (COVID-19)
+            polymerase chain reaction (PCR/RT-PCR/qPCR) viral nucleic acid detection assay (NAA).
+            The renegadeXP™ and CDC tests are permitted and authorized, respectively, during the
+            current emergency declaration in which circumstances exist justifying the authorization
+            of the emergency use of in vitro diagnostic tests for detection of SARS-CoV-2 virus and
+            diagnosis of COVID-19 infection under section 564(b)(1) of the Act, 21 U.S.C.
+            360bbb-3(b)(1), unless the authorization is terminated or revoked sooner.
+          </p>
+          <p class="p4">
+            For more information, please <a href="/support/contact" target="_self">contact us</a>.
+          </p>
+        </div>
+
+        <div id="accreditations-for-renegadebio" class="sub-section copy-block">
+          <h4 class="h4">Accreditations for renegade.bio</h4>
+
+          <ul class="link-list">
+            <li>
+              <a
+                href="https://res.cloudinary.com/renegade-bio/image/upload/v1662585368/pdfs/CLIA-certificate-renegade-bio-Heinz.pdf"
+                rel="noopener"
+                target="_blank"
+                >CLIA License</a
+              >
+            </li>
+            <li>
+              <a
+                href="hhttps://res.cloudinary.com/renegade-bio/image/upload/v1662585368/pdfs/CDPH-renegade-bio-SARS-CoV-2.pdf"
+                rel="noopener"
+                target="_blank"
+                >State of California Certification</a
+              >
+            </li>
+            <li>
+              <a href="http://www.nglcc.org/" rel="noopener" target="_blank"
+                >NGLCC Certified LGBT Business Enterprise</a
+              >
+            </li>
+          </ul>
+        </div>
+
+        <div id="clia-laboratory" class="sub-section copy-block">
+          <h4 class="h4">CLIA Laboratory</h4>
+          <p class="p4">
+            The Clinical Laboratory Improvement Amendments of 1988 (CLIA) regulations include
+            federal standards applicable to all U.S. facilities or sites that test human specimens
+            for health assessment or to diagnose, prevent, or treat disease. The CDC, in partnership
+            with CMS and FDA, supports the CLIA program and clinical laboratory quality.
+          </p>
+        </div>
+
+        <div id="cap-accredited" class="sub-section copy-block">
+          <h4 class="h4">CAP Accredited</h4>
+          <p class="p4">
+            CAP accredits laboratories perform tests using methodologies and clinical application
+            within the expertise of the College of American Pathologist program. Laboratories must
+            be appropriately licensed to perform testing when required by law. CAP accreditation
+            meets or exceeds requirements from CLIA, FDA and OSHA, strengthening patient care and
+            safety.
+          </p>
+        </div>
+        <!-- eslint-enable -->
       </div>
     </section>
   </div>
 </template>
 
 <script setup lang="ts">
-  // import { IPageData } from '@/types/general'
+  import Image from '@/components/Image.vue'
 
-  // const parent: IPageData = {
-  //   id: 'how-we-work',
-  //   title: 'How We Work',
-  // }
-
-  // const section: IPageData = {
-  //   id: 'who-we-are',
-  //   title: 'Who We Are',
-  // }
-
-  const pageName = 'Accreditations'
+  const signature = {
+    id: 'craig-rouskey-signature',
+    src: 'https://res.cloudinary.com/renegade-bio/image/upload/graphics/craig-rouskey-signature',
+    title: 'Craig Rouskey, Co-founder & CEO',
+    alt: 'Craig Rouskey signature',
+    width: 448,
+    height: 151,
+  }
 </script>
+
+<style setup scoped lang="scss">
+  .image-container {
+    flex: 1 1 100%;
+    // img {
+    //   width: ;
+    //   height: ;
+    // }
+  }
+</style>
